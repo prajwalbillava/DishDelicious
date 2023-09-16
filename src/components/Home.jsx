23817baa3f16a063e5d3cf217252884ab7388ff8
Prajwal Poojary <prajwalgpoojary999@gmail.com>
2023-09-16 18:43:28 +0530

    about page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -35,26 +35,53 @@ function Home() {
     <>
       <div className="video-container">
         <img
-          src="https://i.ibb.co/mJnpw1J/homeimage.jpg"
+          src="https://images.unsplash.com/photo-1494859802809-d069c3b71a8a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjJ8fGZvb2R8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"
           className="faded-image"
         ></img>
 
-        <div className="titlemain text-overlay">
+        {/*<div className="titlemain text-overlay">
           <h1 style={{ marginTop: "10px" }} className="dish">
             DISH
           </h1>
           <h1 style={{ marginTop: "10px" }} className="dish">
             DELICIOUS
           </h1>
+  </div>*/}
+      </div>
+      <div className="maintitle">
+        <div className="waviy">
+          <span style={{ "--i": 1 }}>D</span>
+          <span style={{ "--i": 2 }}>I</span>
+          <span style={{ "--i": 3 }}>S</span>
+          <span style={{ "--i": 4 }}>H</span>
+        </div>
+        <div className="waviy">
+          <span style={{ "--i": 5 }}>D</span>
+          <span style={{ "--i": 6 }}>E</span>
+          <span style={{ "--i": 7 }}>L</span>
+          <span style={{ "--i": 8 }}>I</span>
+          <span style={{ "--i": 9 }}>C</span>
+          <span style={{ "--i": 10 }}>I</span>
+          <span style={{ "--i": 11 }}>O</span>
+          <span style={{ "--i": 12 }}>U</span>
+          <span style={{ "--i": 13 }}>S</span>
         </div>
       </div>
 
       {error && alert(error)}
 
       <div class="wrapper1">
-        <h2 className="h2home">
-          Find your perfect meal with our recipe search and planning app
-        </h2>
+        <h1 className="animh1">
+          <span>Find </span>
+          <span>your </span>
+          <span>perfect </span>
+          <span>meal </span>
+          <span>with </span>
+          <span>our </span>
+          <span>recipe </span>
+          <span>search </span>
+          <span>app.</span>
+        </h1>
         <div class="containerh">
           <h1 class="title">Our Services</h1>
           <div class="inner-wrapper">
--- src/components/HomeCard.jsx
+++ src/components/HomeCard.jsx
@@ -10,9 +10,9 @@ function HomeCard(props) {
   let displaycontent = "";
   let url = "";
   const imgary = [
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRfpZB0_3qGRT0vx7Jlw662goIgQc9en4esg&usqp=CAU",
+    "https://images.unsplash.com/photo-1539136788836-5699e78bfc75?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjZ8fGZvb2R8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
     "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8fA%3D%3D&w=1000&q=80",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrdINYIdRCS9B_lqmjbui9rt6kEcsIdaaExQ&usqp=CAU",
+    "https://images.unsplash.com/photo-1542838132-92c53300491e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OTV8fGZvb2R8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
   ];
   const content = [
     "Use our powerful recipe search feature to discover a wide variety of delicious dishes from around the world, tailored to your taste and dietary preferences.",
